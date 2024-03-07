37e5a5c00d0da9f884a8a3f86b0684a427707655
Pankaj Kamble <63709568+diablo7431@users.noreply.github.com>
2024-03-07 10:19:20 +0530

    Profile edit button set

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,49 @@
 import React, { useState } from "react";
-import {getAuth} from "firebase/auth";
+import { getAuth, updateCurrentUser, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore"
+import { db } from "../firebase"
 
 export default function Profile() {
-  const auth =getAuth()
+  const auth = getAuth();
   const [FormData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
   const { name, email } = FormData;
   const navigate = useNavigate();
-  function onLogout(){
-    auth.signOut()
-    navigate('/sign-in')
+  const [changeDetails, setChangeDetails] = useState(false);
+  function onLogout() {
+    auth.signOut();
+    navigate("/sign-in");
+  }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  async function onsubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update Display Name in firebase Auth.
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        //update Display Name in firebase Auth.
+        const dcoRef = doc(db, "users", auth.currentUser.uid)
+        await updateDoc(dcoRef, {
+          name,
+
+
+        })
+      }
+      toast.success("Profile Updated");
+    } catch (error) {
+      toast.error("Could not update details");
+    }
   }
   return (
     <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -24,36 +55,50 @@ export default function Profile() {
             type="text"
             id="name"
             value={name}
-            disabled
-            className="w-full px-5 py-2 text-xl rounded-2xl 
+            disabled={!changeDetails}
+            onChange={onChange}
+            className={`w-full px-5 py-2 text-xl rounded-2xl 
           text-gray-600 border-gray-400 
-          transition ease-in-out cursor-text 
-          mb-5"
+          transition ease-in-out cursor-text mb-5
+          ${changeDetails && "bg-red-100 focus:bg-red-200"}
+          `}
           />
           <input
             type="text"
             id="email"
             value={email}
             disabled
-            className="w-full px-5 py-2 text-xl rounded-2xl 
+            className={`w-full px-5 py-2 text-xl rounded-2xl 
           text-gray-600 border-gray-400 
-          transition ease-in-out cursor-text
-          mb-5
-          "
+          transition ease-in-out cursor-text mb-5
+          `}
           />
-          <div className="flex justify-between whitespace-nowrap 
-          text-m sm:text-lg mb-6 mx-1">
+          <div
+            className="flex justify-between whitespace-nowrap 
+          text-m sm:text-l mb-6 mx-1"
+          >
             <p className="flex items-center">
               Do you want to change your Display name?
-              <span className="text-red-500 hover:text-red-600 
+              <span
+                onClick={() => {
+                  changeDetails && onsubmit();
+                  setChangeDetails((prevState) => !prevState);
+                }}
+                className="text-red-500 hover:text-red-600 
               transition ease-in-out duration-500 cursor-pointer
-              ml-1">Edit</span>
+              ml-1"
+              >
+                {changeDetails ? "Apply Changes?" : "Edit"}
+              </span>
             </p>
-            <p onClick={onLogout} className="text-blue-500 hover:text-blue-600 cursor-pointer
-            transition ease-in-out duration-500">
+            <p
+              onClick={onLogout}
+              className="text-blue-500 hover:text-blue-600 cursor-pointer
+            transition ease-in-out duration-500"
+            >
               Sign Out
             </p>
-          </div>  
+          </div>
         </form>
       </div>
     </section>
